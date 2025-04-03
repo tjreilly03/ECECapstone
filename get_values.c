14f53a0a0c1f05f682321916f49d4689512309c4
Tyler Reilly <tyler.reilly@maine.edu>
2025-04-03 14:32:26 -0400

    getting a new function file get values which will be where the functions for getting potentiometer and the switch and button states will live

---
+++ get_values.c
@@ -0,0 +1,16 @@
+/*!
+ * @file
+ * 
+ * @brief Running Mean Filter Implementation
+ * 
+ * @author ECE486 Lab Group 12
+ * @author Tyler Reilly, Joshua Deveau, Basel Alsarraf
+ * 
+ * @date Feb 2024
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "get_values.h"
+#include <math.h>
---
+++ get_values.h
@@ -0,0 +1,22 @@
+/*!
+ * @file soft_clip.h
+ * 
+ * @brief This will hold the function defs for the soft clipping portion of the pedal.
+ * 
+ * @author Tyler Reilly
+ * 
+ * @date April 2025
+ * 
+ * @defgroup Function Interface Requirements for Soft Clip Filters
+ *   @{
+ * 
+ * 
+      }@
+ */
+
+
+#ifndef GET_VALUES
+#define GET_VALUES
+
+
+#endif
